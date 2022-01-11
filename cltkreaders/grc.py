365bbc79ea7876373d7d115c084fece37ae67212
Patrick J. Burns <patrick@diyclassics.org>
2022-01-10 19:03:35 -0500

    Add POS tagger to Tesserae readers

--- cltkreaders/grc.py
+++ cltkreaders/grc.py
@@ -4,13 +4,17 @@
 __author__ = ["Patrick J. Burns <patrick@diyclassics.org>",]
 __license__ = "MIT License."
 
-from typing import Callable
+from typing import Callable, Iterator, Union
 
 from cltkreaders.readers import TesseraeCorpusReader
 
+from cltk import NLP
+from cltk.core.data_types import Pipeline
+from cltk.languages.utils import get_lang
+from cltk.alphabet.processes import GreekNormalizeProcess
+from cltk.dependency.processes import GreekStanzaProcess
 from cltk.sentence.grc import GreekRegexSentenceTokenizer
 from cltk.tokenizers.word import PunktWordTokenizer as GreekWordTokenizer
-
 
 class GreekTesseraeCorpusReader(TesseraeCorpusReader):
     """
@@ -29,6 +33,11 @@ class GreekTesseraeCorpusReader(TesseraeCorpusReader):
         :param kwargs: Miscellaneous keyword arguments
         """
         self.lang = lang
+        pipeline = Pipeline(description="Greek pipeline for Tesserae readers", 
+                            processes=[GreekNormalizeProcess, GreekStanzaProcess], 
+                            language=get_lang(self.lang))
+        self.nlp = NLP(language=lang, custom_pipeline=pipeline, suppress_banner=True)
+
         if not word_tokenizer:
             self.word_tokenizer = GreekWordTokenizer()
         if not sent_tokenizer:
@@ -37,3 +46,11 @@ class GreekTesseraeCorpusReader(TesseraeCorpusReader):
         TesseraeCorpusReader.__init__(self, root, fileids, encoding, self.lang,
                                       word_tokenizer=self.word_tokenizer,
                                       sent_tokenizer=self.sent_tokenizer)
+    
+    def pos_sents(self, fileids: Union[list, str] = None, preprocess: Callable = None) -> Iterator[list]:
+        for sent in self.sents():
+            data = self.nlp.analyze(text=sent)
+            pos_sent = []
+            for item in data:
+                pos_sent.append(f"{item.string}/{item.upos}")
+            yield pos_sent                                             
