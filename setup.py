574b999f4ff9b31e1272f7f13149a97c43b35753
Patrick J. Burns <patrick@diyclassics.org>
2022-02-10 11:54:02 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup
 
 setup(
     name='cltk_readers',
+<<<<<<< HEAD
     version='0.2.1',
+=======
+    version='0.2.0',
+>>>>>>> 3bdd00f... Update version; update readme, setup
     packages=['cltkreaders'],
     url='https://github.com/diyclassics/cltk_readers',
     license='MIT License',
