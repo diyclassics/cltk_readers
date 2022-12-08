6d63247a8a8a66455c561a1d278cef956eb62422
Patrick J. Burns <patrick@diyclassics.org>
2022-12-08 15:09:23 -0500

    Update version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='cltk_readers',
-    version='0.4.2',
+    version='0.4.3',
     packages=['cltkreaders'],
     url='https://github.com/diyclassics/cltk_readers',
     license='MIT License',
     author='Patrick J. Burns',
     author_email='patrick@diyclassics.org',
     description='Corpus reader extension for the Classical Language Toolkit ',
-    install_requires=['cltk~=1.1.1',
+    install_requires=['cltk~=1.1.5',
                       'lxml==4.9.1', 
                       'pyuca==1.2',
                       'la-core-cltk-sm @ https://github.com/diyclassics/latin-spacy-models/blob/main/la_core_cltk_sm/la_core_cltk_sm-0.1.0.tar.gz?raw=true',
