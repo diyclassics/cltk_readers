e762708f40f6101bd006dd02e9ae9493d4acc5cb
Patrick J. Burns <patrick@diyclassics.org>
2023-01-04 21:43:20 -0500

    Fix words in Greek Tesserae reader

--- cltkreaders/grc.py
+++ cltkreaders/grc.py
@@ -1,7 +1,9 @@
 """Corpus readers to support Greek text collections for use with CLTK; see readers.py for more information
 """
 
-__author__ = ["Patrick J. Burns <patrick@diyclassics.org>",]
+__author__ = [
+    "Patrick J. Burns <patrick@diyclassics.org>",
+]
 __license__ = "MIT License."
 
 import os.path
@@ -22,14 +24,23 @@ from cltk.utils import get_cltk_data_dir, query_yes_no
 from cltk.data.fetch import FetchCorpus
 from cltk.core.exceptions import CLTKException
 
+
 class GreekTesseraeCorpusReader(TesseraeCorpusReader):
     """
     A corpus reader for Greek texts from the Tesserae-CLTK corpus
     """
 
-    def __init__(self, root: str = None, fileids: str = r'.*\.tess', encoding: str = 'utf-8', lang: str = 'grc',
-                 normalization_form: str = 'NFC',
-                 word_tokenizer: Callable = None, sent_tokenizer: Callable = None, **kwargs):
+    def __init__(
+        self,
+        root: str = None,
+        fileids: str = r".*\.tess",
+        encoding: str = "utf-8",
+        lang: str = "grc",
+        normalization_form: str = "NFC",
+        word_tokenizer: Callable = None,
+        sent_tokenizer: Callable = None,
+        **kwargs,
+    ):
         """
         :param root: Location of plaintext files to be read into corpus reader
         :param fileids: Pattern for matching files to be read into corpus reader
@@ -48,9 +59,11 @@ class GreekTesseraeCorpusReader(TesseraeCorpusReader):
 
         self.__check_corpus()
 
-        pipeline = Pipeline(description="Greek pipeline for Tesserae readers", 
-                            processes=[GreekNormalizeProcess, GreekStanzaProcess], 
-                            language=get_lang(self.lang))
+        pipeline = Pipeline(
+            description="Greek pipeline for Tesserae readers",
+            processes=[GreekNormalizeProcess, GreekStanzaProcess],
+            language=get_lang(self.lang),
+        )
         self.nlp = NLP(language=lang, custom_pipeline=pipeline, suppress_banner=True)
 
         if not word_tokenizer:
@@ -63,26 +76,32 @@ class GreekTesseraeCorpusReader(TesseraeCorpusReader):
         else:
             self.sent_tokenizer = sent_tokenizer
 
-        TesseraeCorpusReader.__init__(self, self.root, fileids, encoding, self.lang,
-                                      word_tokenizer=self.word_tokenizer,
-                                      sent_tokenizer=self.sent_tokenizer)
+        TesseraeCorpusReader.__init__(
+            self,
+            self.root,
+            fileids,
+            encoding,
+            self.lang,
+            word_tokenizer=self.word_tokenizer,
+            sent_tokenizer=self.sent_tokenizer,
+        )
 
     @property
     def root(self):
         if not self._root:
             self._root = os.path.join(
-                            get_cltk_data_dir(),
-                            f"{self.lang}/text/{self.corpus}/texts")
+                get_cltk_data_dir(), f"{self.lang}/text/{self.corpus}/texts"
+            )
         return self._root
 
     def __check_corpus(self):
         if not os.path.isdir(self.root):
             if self.root != os.path.join(
-                    get_cltk_data_dir(),
-                    f"{self.lang}/text/{self.corpus}/texts"):
+                get_cltk_data_dir(), f"{self.lang}/text/{self.corpus}/texts"
+            ):
                 raise CLTKException(
                     f"Failed to instantiate GreekTesseraeCorpusReader. Root folder not found."
-                )                        
+                )
             else:
                 print(  # pragma: no cover
                     f"CLTK message: Unless a path is specifically passed to the 'root' parameter, this corpus reader expects to find the CLTK-Tesserae texts at {f'{self.lang}/text/{self.lang}_text_tesserae/texts'}."
@@ -92,33 +111,41 @@ class GreekTesseraeCorpusReader(TesseraeCorpusReader):
                 )  # type: bool
                 if dl_is_allowed:
                     fetch_corpus = FetchCorpus(language=self.lang)
-                    fetch_corpus.import_corpus(
-                        corpus_name=self.corpus
-                    )
-                    fetch_corpus.import_corpus(corpus_name=f'{self.lang}_models_cltk')
+                    fetch_corpus.import_corpus(corpus_name=self.corpus)
+                    fetch_corpus.import_corpus(corpus_name=f"{self.lang}_models_cltk")
                 else:
                     raise CLTKException(
-                        f"Failed to instantiate GreekTesseraeCorpusReader. Rerun with 'root' parameter set to folder with .tess files or download the corpus to the CLTK_DATA folder."                                      
+                        f"Failed to instantiate GreekTesseraeCorpusReader. Rerun with 'root' parameter set to folder with .tess files or download the corpus to the CLTK_DATA folder."
                     )
 
-    def docs(self, fileids: Union[list, str] = None) -> Iterator[str]: 
+    def docs(self, fileids: Union[list, str] = None) -> Iterator[str]:
         """
         :param fileids: Subset of files to be processed by reader tasks
         :yield: Plaintext content of file
         """
         for path, encoding in self.abspaths(fileids, include_encoding=True):
-            with codecs.open(path, 'r', encoding=encoding) as f:
+            with codecs.open(path, "r", encoding=encoding) as f:
                 doc = f.read()
                 doc = doc.strip()
-                yield doc     
+                yield doc
 
-    def pos_sents(self, fileids: Union[list, str] = None, preprocess: Callable = None) -> Iterator[list]:
-        for sent in self.sents(fileids):
+    def pos_sents(
+        self, fileids: Union[list, str] = None, preprocess: Callable = None
+    ) -> Iterator[list]:
+        for sent in self.sents(fileids, preprocess=preprocess):
             data = self.nlp.analyze(text=sent)
             pos_sent = []
             for item in data:
                 pos_sent.append(f"{item.string}/{item.upos}")
-            yield pos_sent                                             
+            yield pos_sent
+
+    def words(
+        self, fileids: Union[list, str] = None, preprocess: Callable = None
+    ) -> Iterator[list]:
+        for pos_sent in self.pos_sents(fileids, preprocess=preprocess):
+            for item in pos_sent:
+                yield item.split("/")[0]
+
 
 # TODO: Add corpus download support following Tesserae example
 GreekPerseusTreebankCorpusReader = PerseusTreebankCorpusReader
